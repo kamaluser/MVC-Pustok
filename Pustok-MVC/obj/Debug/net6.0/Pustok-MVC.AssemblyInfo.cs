330429e9cce1ab2c5f45110874c08f8f1ebd8218
Huseynaliyev Kamal <kamalrh@code.local>
2024-04-25 18:21:15 +0400

    modal done

--- Pustok-MVC/obj/Debug/net6.0/Pustok-MVC.AssemblyInfo.cs
+++ Pustok-MVC/obj/Debug/net6.0/Pustok-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pustok-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc3f9074905ffbf1813ea60d417e6905638dae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pustok-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pustok-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Pustok-MVC/wwwroot/js/main.js
@@ -0,0 +1,24 @@
+﻿namespace Pustok_MVC.wwwroot.js
+{
+    public class main
+    {
+        document.addEventListener("DOMContentLoaded", function() {
+            const bookModals = document.querySelectorAll(".book-modal");
+
+            bookModals.forEach(function (modal) {
+                modal.addEventListener("click", function (e) {
+                    e.preventDefault();
+                    const url = this.getAttribute("href");
+
+                    fetch(url)
+                        .then(response => response.text())
+                        .then(data => {
+                            document.querySelector("#quickModal .modal-dialog").innerHTML = data;
+                        });
+
+                    document.querySelector("#quickModal").classList.add("show");
+                });
+            });
+        });
+    }
+}
